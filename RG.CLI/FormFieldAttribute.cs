eb453f5a8e9a32d3a1ff001befb94c318f40f6cd
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2019-11-28 17:17:48 +0700

    Version 1.0.3: Added FormInput

---
+++ RG.CLI/FormFieldAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RG.CLI {
+	[AttributeUsage(AttributeTargets.Property)]
+	public class FormFieldAttribute : Attribute {
+		public string? DisplayName { get; }
+		public bool NullIfEmpty { get; set; }
+
+		public FormFieldAttribute(string? displayName = null) {
+			DisplayName = displayName;
+		}
+	}
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,7 +8,17 @@ namespace TestApp {
 			app["hello"] = args => Console.WriteLine("Hello world!");
 			app["hello {name}"] = args => Console.WriteLine($"Hello, {args[0]}!");
 			app["hello kitty"] = args => throw new NotImplementedException();
+			app["form"] = args => Console.WriteLine(FormInput.ReadForm<Person>());
 			app.Run();
 		}
+	}
+
+	class Person {
+		public string Name { get; set; }
+
+		[FormField("Gender (M/F)")]
+		public string Gender { get; set; }
+
+		public override string ToString() => $"{Name} ({Gender})";
 	}
 }
